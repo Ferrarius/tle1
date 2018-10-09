042cedc3093266389d3e8783731dbbaa244180d0
Gijs Bijvoets <0950640@hr.nl>
2018-10-09 14:57:43 +0200

    Added Title to home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MotDatKosten
                 </div>
 
                 <div class="links">
