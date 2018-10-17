a761e8e0a5809e3934b444ac87e4fb1ded43de2f
katjang <niek_o@hotmail.com>
2018-10-17 14:42:19 +0200

    made some basic views for reports

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,9 +55,10 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a href="{{route('user.reports')}}" class="dropdown-item">My Reports</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
