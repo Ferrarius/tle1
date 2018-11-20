6e149b33dbc55f83f2a203306fd8e64b03cdd70d
Gijs Bijvoets <0950640@hr.nl>
2018-11-20 15:26:19 +0100

    changed the index to full image page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,28 +1,34 @@
 @extends('layouts.main')
 
 @section('content')
-        <div class="py-5 container">
-            <div class="jumbotron text-lg-center">
-                <form class="py-5">
-                    <div class="form-group d-inline-block mr-3 text-left">
-                        <label for="inputZip">Postcode</label>
-                        <input type="text" class="form-control" id="inputZip" placeholder="Postcode" required>
-                    </div>
-                    <div class="form-group d-inline-block mr-3 text-left">
-                        <label for="inputHousenumber">Huisnummer</label>
-                        <input type="number" class="form-control" id="inputHousenumber" placeholder="Huisnummer" required>
-                    </div>
-                    <div class="form-group d-inline-block mr-3 text-left">
-                        <label for="inputAddition">Toevoeging</label>
-                        <input type="text" class="form-control" id="inputAddition" placeholder="Toevoeging">
-                    </div>
-                    <div class="form-group d-inline-block mr-3 text-left">
-                        <label for="inputAmount">Beschikbaar bedrag</label>
-                        <input type="number" class="form-control" id="inputAmount" placeholder="Bedrag">
-                    </div>
-                    <button type="submit" class="btn btn-primary btn-green mb-1">DOE DE CHECK →</button>
-                </form>
-            </div>
+
+    <div id="hero-img" class="">
+        <div class=" container col-6 offset-3 bg-light card d-flex align-items-center" id="form-home">
+            <form class="py-5">
+                <div class="form-group d-inline-block mr-3 text-left">
+                    <label for="inputZip">Postcode</label>
+                    <input type="text" class="form-control" id="inputZip" placeholder="Postcode" required>
+                </div>
+                <div class="form-group d-inline-block mr-3 text-left">
+                    <label for="inputHousenumber">Huisnummer</label>
+                    <input type="number" class="form-control" id="inputHousenumber" placeholder="Huisnummer" required>
+                </div>
+                <div class="form-group d-inline-block mr-3 text-left">
+                    <label for="inputAddition">Toevoeging</label>
+                    <input type="text" class="form-control" id="inputAddition" placeholder="Toevoeging">
+                </div>
+                <div class="form-group d-inline-block mr-3 text-left">
+                    <label for="inputAmount">Beschikbaar bedrag</label>
+                    <input type="number" class="form-control" id="inputAmount" placeholder="Bedrag">
+                </div>
+                <button type="submit" class="btn btn-primary btn-green mb-1">DOE DE CHECK →</button>
+            </form>
         </div>
+    </div>
+
+
+
+
+
 
 @endsection
